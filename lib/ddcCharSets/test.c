0c6beca67d31aa385501b20425316ce8c56c6463
smokeythekat <owr5273@gmail.com>
2021-02-26 18:58:16 -0400

    fixed lib

--- lib/ddcCharSets/test.c
+++
@@ -1,5 +0,0 @@
-#include "ddcCharSets.h"
-int main(void)
-{
-	return 0;
-}
--- lib/ddcMath/test.c
+++
@@ -1,9 +0,0 @@
-#include "ddcMath.h"
-#include <ddcString.h>
-#include <ddcPrint.h>
-
-int main(void)
-{
-	fltPrint(sq2(8));
-	return 0;
-}
